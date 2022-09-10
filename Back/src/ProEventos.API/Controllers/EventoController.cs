a1e45c9e49dcfbe22be8201866c7166f5788d6f0
vitorjr <vitorufc2013@gmail.com>
2022-09-09 22:14:29 -0300

    Adicionando primeiras classes do projeto

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+
+
+        public EventoController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {   
+            return new Evento(){
+                EventoId =1,
+                Tema = "Criando projeto",
+                Local = "Fortaleza",
+                QtdPessoas = 250,
+                Lote = "!º Lote",
+                DataEvento = DateTime.Now.AddDays(5).ToString()
+            };
+        }
+    }
+}

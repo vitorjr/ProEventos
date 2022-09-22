c22d04ca551c233b8a4d2a6e11128c9a7e5dc358
vitorjr <vitorufc2013@gmail.com>
2022-09-21 22:59:42 -0300

    Adicionando Filtro e imagens na tela de Eventos

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public eventosFiltrados : any = [];
+  larguraImagem : number = 150;
+  margemImagem : number = 2;
+  mostrarImagem : boolean = true;
+  private _filtroLista : string = '';
+
+  public get filtroLista() : string{
+    return this._filtroLista;
+  }
+
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarpor:string): any{
+    filtrarpor = filtrarpor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento:any) => evento.tema.toLocaleLowerCase().indexOf(filtrarpor) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filtrarpor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,9 +38,16 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  alterarImagem(){
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
   public getEventos(): void{
     this.http.get('https://localhost:5001/api/evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos;
+      },
       error =>  console.log(error)
     );
 
